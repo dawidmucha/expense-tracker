cdaca6936c702b76cc50c2bc7fe4204d12c45314
Dawid Mucha <dawidmucha98@gmail.om>
2022-08-11 13:59:10 +0200

    logging out

---
+++ src/components/LogoutForm.vue
@@ -0,0 +1,23 @@
+<script>
+import { getAuth, signOut } from 'firebase/auth'
+
+export default {
+	name: 'LogoutForm',
+	methods: {
+		onLogoutUser() {
+			const auth = getAuth()
+			const user = auth.currentUser
+			console.log('logging out user', user)
+
+			signOut(auth).then(() => {
+				console.log('logging out')
+				this.$router.push('/')
+			}).catch(e => console.error(e))
+		}
+	}
+}
+</script>
+
+<template>
+	<button @click="onLogoutUser">Log out</button>
+</template>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,5 +1,23 @@
+<script>
+import { getAuth } from 'firebase/auth'
+import LogoutForm from '@/components/LogoutForm.vue'
+
+export default {
+  name: 'DashboardView',
+  components: {
+    LogoutForm
+  },
+  data() {
+    return {
+      currentUser: getAuth().currentUser
+    }
+  }
+}
+</script>
+
 <template>
   <div>
     <h1>This is an dashboard page</h1>
+    <LogoutForm v-if="currentUser !== null" />
   </div>
 </template>
