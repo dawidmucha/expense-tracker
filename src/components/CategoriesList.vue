f54b3e6559b6c3f2d72300384560e0293c8d6b2a
Dawid Mucha <dawidmucha98@gmail.om>
2022-08-18 14:44:19 +0200

    subcategories addable

--- src/components/CategoriesList.vue
+++ src/components/CategoriesList.vue
@@ -4,6 +4,7 @@ import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import { ref, reactive } from 'vue'
 import db from '@/main'
 import NewCategoryForm from '@/components/NewCategoryForm.vue'
+import NewSubcatForm from '@/components/NewSubcatForm.vue'
 
 const categories = ref({})
 const uid = ref(undefined)
@@ -15,18 +16,18 @@ onAuthStateChanged(getAuth(), async (user) => {
 	onSnapshot(doc(db, 'categories', uid.value), doc => {
 		if(uid.value !== undefined) {
 			categories.value = doc.data()
-			console.log('adada', doc.data())
 		}
 	})
 })
 </script>
 
 <template>
-	<div v-for="(category, index) in categories" :key="index">
-		<h2>{{ index }}</h2>
+	<h2><NewCategoryForm /></h2>
+	<div v-for="(category, key, index) in categories" :key="index">
+		<h2>{{ key }}</h2>
 		<div v-for="(subcat, index) in category" :key="index">
 			<div>{{ subcat }}</div>
 		</div>
+		<div><NewSubcatForm :category="key" /></div>
 	</div>
-	<h2><NewCategoryForm /></h2>
 </template>
--- src/components/NewSubcatForm.vue
+++ src/components/NewSubcatForm.vue
@@ -7,22 +7,24 @@ import { ref, defineProps } from 'vue'
 const uid = ref(undefined)
 const errMsg = ref(null)
 
+const props = defineProps(["category"])
+
 onAuthStateChanged(getAuth(), async (user) => {
 	if(user && (uid.value === undefined)) uid.value = getAuth().currentUser.uid
 })
 
-const onNewCategoryFormSubmit = async (e) => {
+const onNewSubcatFormSubmit = async (e) => {
 	if(uid.value === undefined) return undefined
-	const categoryName = e.target.categoryName.value
+	const subcatName = e.target.subcatName.value
+	e.target.subcatName.value = ''
 	let newData = {}
 
 	const docRef = doc(db, 'categories', uid.value)
 	const docSnap = await getDoc(docRef)
 	if(docSnap.exists()) {
-		newData = {
-			...docSnap.data(),
-			[categoryName]: []
-		}
+		newData = { ...docSnap.data() }
+		const categoryName = props.category
+		newData[categoryName].push(subcatName)
 	}
 
 	setDoc(docRef, newData)
@@ -30,8 +32,8 @@ const onNewCategoryFormSubmit = async (e) => {
 </script>
 
 <template>
-	<form @submit.prevent="(e) => onNewCategoryFormSubmit(e)">
-		<input type="text" id="categoryName" name="categoryName">
+	<form @submit.prevent="(e) => onNewSubcatFormSubmit(e)">
+		<input type="text" id="subcatName" name="subcatName">
 		<input type="submit" value="+" /> {{ errMsg }}
 	</form>
 </template>
